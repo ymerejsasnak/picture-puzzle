9cc533c04c098f5cb2802a6c87ca64999fdfd008
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-09-17 13:41:32 -0400

    got it to load image in grid of separate divs

---
+++ pic.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$(function() {
+
+    var PIC_SIZE = 600;
+    var DIFFICULTY = 4;
+    var CELL_SIZE = PIC_SIZE / DIFFICULTY;
+
+   
+    var pictureDiv = $('#picture');
+    var grid = initGrid();
+    
+    
+
+    function slicePicture() {
+        var cell;
+        for(var row = 0; row < DIFFICULTY; row++) {
+            for (var col = 0; col < DIFFICULTY; col++) {
+                cell = $('<div id="' + (col + DIFFICULTY * row) + '" class="cell"></div>')
+                cell.css({'background': 'url("photo.JPG")', 
+                          'background-size': PIC_SIZE + 'px ' + PIC_SIZE + 'px',
+                          'background-position': (-PIC_SIZE / DIFFICULTY * col) + 'px ' + (-PIC_SIZE / DIFFICULTY * row) + 'px'});
+                cell.width(PIC_SIZE / DIFFICULTY);
+                cell.height(PIC_SIZE / DIFFICULTY);
+                pictureDiv.append(cell);
+            }
+        }
+    }
+
+    function initGrid() {
+        var grid = [];
+        for (var i = 0; i < DIFFICULTY * DIFFICULTY; i++) {
+            grid.push(0);
+        }
+        return grid;
+    }
+
+   
+
+   
+
+    slicePicture();
+
+    console.log(grid)
+    
+
+
+
+    
+
+});
